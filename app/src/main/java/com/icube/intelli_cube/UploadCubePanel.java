ef69637883ce3a7235dbe5db9223bf20f545ab91
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2018-07-17 06:02:35 +0100

    Upload Cube Panel
    Signed-off-by: Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>

---
+++ app/src/main/java/com/icube/intelli_cube/UploadCubePanel.java
@@ -0,0 +1,13 @@
+package com.icube.intelli_cube;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UploadCubePanel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_cube_panel);
+    }
+}
